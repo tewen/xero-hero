abe5ad6e96511ce00f18aeef83e4ce15c786ddcb
Trevor Ewen <trevor@southporttechnologygroup.com>
2023-10-07 16:49:03 -0400

    Create invoice attachment, no unit tests.

--- src/accounting/contacts/__tests__/links.test.ts
+++ src/accounting/contacts/__tests__/links.test.ts
@@ -7,36 +7,36 @@ describe('contacts/links', () => {
     it('returns a null indicator if passed undefined', () => {
       // @ts-expect-error - This is an invalid type for the function.
       expect(getContactLink()).toBe(
-        'https://go.xero.com/app/!kR4N6/contacts/contact/null-or-empty-contact-id',
+        'https://go.xero.com/Contacts/View.aspx?contactID=null-or-empty-contact-id',
       );
     });
 
     it('returns a null indicator if passed null', () => {
       // @ts-expect-error - This is an invalid type for the function.
       expect(getContactLink(null)).toBe(
-        'https://go.xero.com/app/!kR4N6/contacts/contact/null-or-empty-contact-id',
+        'https://go.xero.com/Contacts/View.aspx?contactID=null-or-empty-contact-id',
       );
     });
 
     it('returns a null indicator if passed an empty string', () => {
       expect(getContactLink('')).toBe(
-        'https://go.xero.com/app/!kR4N6/contacts/contact/null-or-empty-contact-id',
+        'https://go.xero.com/Contacts/View.aspx?contactID=null-or-empty-contact-id',
       );
     });
 
-    it('returns the correct URL for an invoice object with an invoiceID property', () => {
+    it('returns the correct URL for an contact object with an contactID property', () => {
       const contact = new Contact();
       /* eslint-disable functional/immutable-data */
       contact.contactID = '25OR6TO4';
       /* eslint-enable functional/immutable-data */
       expect(getContactLink(contact)).toBe(
-        'https://go.xero.com/app/!kR4N6/contacts/contact/25OR6TO4',
+        'https://go.xero.com/Contacts/View.aspx?contactID=25OR6TO4',
       );
     });
 
     it('returns the correct URL for a string invoice ID', () => {
       expect(getContactLink('kooolaid-9086-t728')).toBe(
-        'https://go.xero.com/app/!kR4N6/contacts/contact/kooolaid-9086-t728',
+        'https://go.xero.com/Contacts/View.aspx?contactID=kooolaid-9086-t728',
       );
     });
 
@@ -45,7 +45,7 @@ describe('contacts/links', () => {
         contactID: '25OR6TO4',
       };
       expect(getContactLink(contact)).toBe(
-        'https://go.xero.com/app/!kR4N6/contacts/contact/25OR6TO4',
+        'https://go.xero.com/Contacts/View.aspx?contactID=25OR6TO4',
       );
     });
   });
--- src/accounting/contacts/links.ts
+++ src/accounting/contacts/links.ts
@@ -1,11 +1,13 @@
+import qs from 'qs';
 import type { Contact } from 'xero-node';
 
 import { hasProperty } from '../../utils/properties';
 
 export const getContactLink = (contact: Contact | string): string => {
-  return `https://go.xero.com/app/!kR4N6/contacts/contact/${
-    (hasProperty(contact, 'contactID')
-      ? (contact as Contact).contactID
-      : contact) || 'null-or-empty-contact-id'
-  }`;
+  return `https://go.xero.com/Contacts/View.aspx?${qs.stringify({
+    contactID:
+      (hasProperty(contact, 'contactID')
+        ? (contact as Contact).contactID
+        : contact) || 'null-or-empty-contact-id',
+  })}`;
 };
